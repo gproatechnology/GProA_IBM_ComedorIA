67be846c4ae9fba11a6686012cedb9e97a038a26
gproatechnology <gproatechnology@gmail.com>
2025-12-19 12:58:14 -0600

    Delete app directory

--- app/config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///comedor.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
